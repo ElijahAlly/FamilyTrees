0a6218bea8280aaf3ad141656734acca1fec1cb8
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2025-02-19 21:31:56 -0500

    updated app name to e dynamic - will need to update again once claiming a family or person logic is done

--- components/family/SearchFamilyTrees.vue
+++ components/family/SearchFamilyTrees.vue
@@ -15,6 +15,10 @@ const userInputedCharAndDidNotClickOnResult = ref(false);
 const families = ref<FamilyType[]>([]);
 const people = ref<PersonType[]>([]);
 
+const searchInputEle = ref<HTMLInputElement | null>(null);
+const selectedIndex = ref(-1);
+const loadingResults = ref(false);
+
 const { title, searchBy } = defineProps({
     title: { type: String, default: '' },
     searchBy: { type: String as PropType<'families' | 'people' | 'all'>, default: 'all', required: false }
@@ -24,7 +28,9 @@ const placeholder = searchBy === 'families' ? 'Enter Family Name (Your last name
 const familiesByName = ref<{familyName: string, familyFromDb: FamilyType}[]>([]);
 
 watch(userInput, async (newInput) => {
-    if (newInput) {
+    selectedIndex.value = 0;
+
+    if (newInput && !loadingResults.value) {
         userInputedCharAndDidNotClickOnResult.value = true;
         try {
             // Already sorted alphabetically from supabase
@@ -77,13 +83,53 @@ watch(userInput, async (newInput) => {
     }
 });
 
+const handleKeydown = (e: KeyboardEvent) => {
+    if (!userInput.value) {
+        selectedIndex.value = -1;
+        return;
+    }
+
+    const items = searchBy === 'families' ? families.value : people.value;
+
+    switch (e.key) {
+        case 'ArrowDown':
+            e.preventDefault();
+            selectedIndex.value = selectedIndex.value < items.length - 1 ? selectedIndex.value + 1 : 0;
+            break;
+        case 'ArrowUp':
+            e.preventDefault();
+            selectedIndex.value = selectedIndex.value > 0 ? selectedIndex.value - 1 : items.length - 1;
+            break;
+        case 'Enter':
+            e.preventDefault();
+            const indexToSearchBy = selectedIndex.value >= 0 ? selectedIndex.value : 0;
+            if (searchBy === 'families') {
+                handleFamilyNameClick(families.value[indexToSearchBy]);
+            } else if (searchBy === 'people') {
+                handlePersonClick(people.value[indexToSearchBy]);
+            }
+            break;
+    }
+}
+
+const setLoadingResultsFalse = () =>  {
+    loadingResults.value = false;
+}
+
 const handleClearInput = () => {
     userInput.value = '';
     userInputedCharAndDidNotClickOnResult.value = false;
+    loadingResults.value = false;
+
+    selectedIndex.value = -1;
+    // Focus the input after clearing
+    searchInputEle.value?.focus();
 }
 
 const handleFamilyNameClick = (family: FamilyType) => {
+    loadingResults.value = true;
     userInputedCharAndDidNotClickOnResult.value = false;
+    userInput.value = family.family_name;
     familyStore.setFamily(family);
     familyStore.setSearchedForFamily(true);
     familiesByName.value.forEach(familyObj => {
@@ -94,8 +140,10 @@ const handleFamilyNameClick = (family: FamilyType) => {
 }
 
 const handlePersonClick = (person: PersonType) => {
+    loadingResults.value = true;
     // To get a list of people with this name, Query the db by first, (and middle if the option clicked on has a middle name) and last name 
     userInputedCharAndDidNotClickOnResult.value = false;
+    userInput.value = getFullName(person) || userInput.value;
     personStore.setPerson(person);
     personStore.setSearchedForPerson(true);
 }
@@ -111,7 +159,10 @@ const handlePersonClick = (person: PersonType) => {
                 :class="`w-3/5 min-w-[160px] rounded-md border border-zinc-950 dark:border-zinc-100 text-[14px] leading-none h-[35px] text-grass11 shadow-sm hover:shadow-md hover:bg-mauve3 focus:shadow-lg data-[placeholder]:text-grass9 outline-none ${userInput.length ? 'shadow-none hover:shadow-none' : ''}`">
                 <div class="relative h-full w-full">
                     <input 
+                        ref="searchInputEle"
                         v-model="userInput"
+                        @keydown="handleKeydown"
+                        @click="setLoadingResultsFalse"
                         class="!bg-transparent px-2 py-1 outline-none text-grass11 h-full w-full selection:bg-grass5 placeholder-mauve8 border-none dark:text-white"
                         :placeholder="placeholder" 
                     />
@@ -137,9 +188,15 @@ const handlePersonClick = (person: PersonType) => {
                                 </NuxtLink>
                             </span>
                         </ComboboxItem>
-                        <ComboboxItem v-else v-for="(family, index) in families" :key="index"
+                        <ComboboxItem 
+                            v-else 
+                            v-for="(family, index) in families" 
+                            :key="index"
                             class="text-[13px] leading-none text-grass11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none hover:bg-neutral-200 dark:hover:bg-neutral-700 data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-grass9 data-[highlighted]:text-grass1"
-                            :value="family.family_name" @click="() => handleFamilyNameClick(family)">
+                            :class="{ 'bg-neutral-200 dark:bg-neutral-700': selectedIndex === index }"
+                            :value="family.family_name" 
+                            @click="() => handleFamilyNameClick(family)"
+                        >
                             <span>
                                 {{ family.family_name }}
                             </span>
@@ -155,9 +212,15 @@ const handlePersonClick = (person: PersonType) => {
                                 </NuxtLink>
                             </span>
                         </ComboboxItem>
-                        <ComboboxItem v-else v-for="(person, index) in people" :key="index"
+                        <ComboboxItem 
+                            v-else 
+                            v-for="(person, index) in people" 
+                            :key="index"
                             class="text-[13px] leading-none text-grass11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none hover:bg-neutral-200 dark:hover:bg-neutral-700 data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-grass9 data-[highlighted]:text-grass1"
-                            :value="getFullName(person)" @click="() => handlePersonClick(person)">
+                            :class="{ 'bg-neutral-200 dark:bg-neutral-700': selectedIndex === index }"
+                            :value="getFullName(person)" 
+                            @click="() => handlePersonClick(person)"
+                        >
                             <span>
                                 {{ getFullName(person) }}
                             </span>
